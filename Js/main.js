0b425bd8d133dba55363eca18d60588c11638b17
agharayee <agharayee@gmail.com>
2020-04-12 23:59:07 +0100

    Done with Mobile

--- Js/main.js
+++ Js/main.js
@@ -1,3 +1,6 @@
-let icon = document.getElementById("icon");
+let cancel = document.getElementById("cancel");
+let headercontent = document.getElementById("header-content");
 
-if ()
+cancel.addEventListener("click", function(){
+    headercontent.style.display = "none";
+})
