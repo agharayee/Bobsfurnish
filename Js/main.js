6fcb2d21418a963780bc0e2517934e28b48b7ac5
agharayee <agharayee@gmail.com>
2020-04-14 20:43:40 +0100

    Navbar Javascript

--- Js/main.js
+++ Js/main.js
@@ -1,6 +1,39 @@
 let cancel = document.getElementById("cancel");
+const icon = document.getElementById('icon');
+const iconClick = document.getElementById("secondNav");
+const ThirdNav = document.getElementById('thirdNav');
 let headercontent = document.getElementById("header-content");
 
 cancel.addEventListener("click", function(){
     headercontent.style.display = "none";
 })
+        if(screen.width > 1100){
+            iconClick.classList.remove("d-none");
+            ThirdNav.classList.remove("d-none");
+        }
+
+icon.addEventListener('click', ()=>
+    {
+        if (icon.click && iconClick.classList.contains("d-none") && ThirdNav.classList.contains("d-none")){
+            iconClick.classList.remove("d-none");
+            ThirdNav.classList.remove("d-none")
+        }
+        else
+        {
+            iconClick.classList.add("d-none");
+            ThirdNav.classList.add("d-none")
+        }
+})
+ 
+// icon.addEventListener('click', ()=>
+// {   
+//     if (icon.click){
+//     iconClick.style.display = 'flex';
+//     ThirdNav.style.display = 'flex';
+// }else if (icon.display = 'flex' && icon.click){
+//     iconClick.style.display = 'none';
+//     ThirdNav.style.display = 'none';
+// }
+    
+
+// } )
